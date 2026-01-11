709fba1180848ffa195f9e66013718991cdf1297
ahmedrehmancode <ahmedrehman0010@gmail.com>
2026-01-12 02:08:14 +0500

    updated

---
+++ CMS(Clinic Management System)/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS_Clinic_Management_System_.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
