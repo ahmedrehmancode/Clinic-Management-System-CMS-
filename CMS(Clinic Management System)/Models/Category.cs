494bc44ec2f58fc3b5e53e1895fcad82e6bb4021
ahmedrehmancode <ahmedrehman0010@gmail.com>
2026-01-14 05:04:50 +0500

    updated

--- CMS(Clinic Management System)/Models/Category.cs
+++ CMS(Clinic Management System)/Models/Category.cs
@@ -1,6 +1,16 @@
-﻿namespace CMS_Clinic_Management_System_.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS_Clinic_Management_System_.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]      
+        public string CategoryName { get; set; }
+
+        
+
+
     }
 }
