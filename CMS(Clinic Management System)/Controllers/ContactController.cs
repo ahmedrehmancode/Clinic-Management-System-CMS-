48273217e136ea194e7e1a86c4a983910f450e41
ahmedrehmancode <ahmedrehman0010@gmail.com>
2026-01-19 05:59:30 +0500

    updated

---
+++ CMS(Clinic Management System)/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS_Clinic_Management_System_.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
